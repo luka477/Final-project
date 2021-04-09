6c12c195a79d38ffb34fcdc366d1d0d338e67637
luka477 <77852789+luka477@users.noreply.github.com>
2021-04-09 14:11:47 +0400

    update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-$("#burger-menu").on("click", function(){
-    $("#menu").toggleClass("open-menu");
-    $("#socicon").toggleClass("open-socicon");
-  })
+const faBars = document.getElementsByClassName('fa-bars')[0]
+const nav = document.getElementsByClassName('nav')[0]
+
+faBars.addEventListeren('click', () => {
+	nav.classList.fa('active')
+})
